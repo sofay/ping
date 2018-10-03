3a92bd6c0f229469e5e7403d3a09c989cf134df3
Fay <nanyang.lf@raycloud.com>
2018-10-03 18:47:55 +0800

    part push

--- src/main/java/cn/fay/ping/App.java
+++ src/main/java/cn/fay/ping/App.java
@@ -9,6 +9,11 @@ import java.io.InputStreamReader;
 import java.net.ConnectException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -20,6 +25,23 @@ public class App {
     private static final Logger LOGGER = LoggerFactory.getLogger("ping");
 
     public static void main(String[] args) throws Exception {
+        final Connection connection = getConnection();
+        if (connection == null) {
+            return;
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                if (connection != null) {
+                    try {
+                        connection.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(App.class.getClassLoader().getResourceAsStream("url.txt")));
         ExecutorService executorService = Executors.newCachedThreadPool();
         String line;
@@ -33,19 +55,23 @@ public class App {
                         name = name.contains("-") ? name.split("-")[0] : name;
                         int port = Integer.parseInt(data.split(":")[1]);
                         while (true) {
+                            int delay = 999;
+                            String msg = "";
                             try {
                                 long start = System.currentTimeMillis();
                                 new Socket(url, port).close();
-                                LOGGER.info("name:{} url:{} cast:{}", name, url, System.currentTimeMillis() - start);
+                                delay = (int) (System.currentTimeMillis() - start);
                             } catch (IOException e) {
                                 if (e instanceof ConnectException) {
-                                    LOGGER.info("name:{} url:{} cast:{} msg:{}", name, url, 999, e.getMessage());
+                                    msg = e.getMessage();
                                 } else if (e instanceof UnknownHostException) {
-                                    LOGGER.info("name:{} url:{} cast:{} msg:{}", name, url, 999, "unknown host");
+                                    msg = "unknown host";
                                 } else {
-                                    e.printStackTrace();
+                                    msg = e.getMessage();
                                 }
                             }
+                            putData(name, url, port, delay, msg, connection);
+                            LOGGER.info("name:{} url:{} cast:{} msg:{}", name, url, delay, msg);
                             try {
                                 Thread.sleep(1000); // 1s
                             } catch (InterruptedException e) {
@@ -57,6 +83,41 @@ public class App {
             }
         } finally {
             reader.close();
+        }
+    }
+
+    private static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
+        return DriverManager.getConnection("jdbc:mysql:///vpn", "root", null);
+    }
+
+    private static void putData(String name, String url, int port, int delay, String msg, Connection connection) {
+        String sql = "insert into delay(time, name, url, port, delay, msg) values(?, ?, ?, ?, ?, ?)";
+        PreparedStatement preparedStatement = null;
+        try {
+            int i = 1;
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setObject(i++, new Date());
+            preparedStatement.setString(i++, name);
+            preparedStatement.setString(i++, url);
+            preparedStatement.setInt(i++, port);
+            preparedStatement.setInt(i++, delay);
+            preparedStatement.setString(i++, msg);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
